6930ffa66115fd1eab73f8306b5ddc21bd0024ea
Gerard <gkhynes@gmail.com>
2018-01-14 16:15:59 +0000

    Add replaceLogo function

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,13 +1,24 @@
+const StandardLogo = document.querySelector(".logo__standard");
+const gifLogo = document.querySelector(".logo__gif");
+const keycode = document.querySelector(".keycode");
+
 const pressed = [];
 const secretCode =
   "ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftArrowRightba";
+
+function replaceLogo() {
+  StandardLogo.classList.add("hidden");
+  keycode.classList.add("hidden");
+  gifLogo.classList.add("active");
+}
+
 window.addEventListener("keyup", e => {
   console.log(e.key);
   pressed.push(e.key);
   pressed.splice(-secretCode.length, pressed.length - secretCode.length);
   if (pressed.join("").includes(secretCode)) {
     console.log("DING DING!");
-    cornify_add();
+    replaceLogo();
   }
   console.log(pressed);
 });
